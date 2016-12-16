d7c344d33c04aa8b56fd9ed2b97c9758e224ec8e
AlekseyAQ <kuznetz.vrn@gmail.com>
2016-12-16 12:07:28 -0500

    add pylint, fix pylint errors

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,7 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-    
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -25,7 +25,7 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
-    
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
